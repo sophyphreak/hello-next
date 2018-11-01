2cc9f76a14ca4feebc002f3c0a0f550110e651db
Andrew <arhorn@smcm.edu>
2018-11-01 14:17:44 +0700

    finished tutorial through fetching data for pages

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6351de92c0987035c461";
+/******/ 	var hotCurrentHash = "d0c7f798bd6155574fa4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
